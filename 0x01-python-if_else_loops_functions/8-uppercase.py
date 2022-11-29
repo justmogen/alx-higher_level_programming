e9654e429cb7f00e1925376782747de61c2ff4a1
justmogen <justmogen@gmail.com>
2022-11-29 21:04:53 +0000

    check for uppercase in ascii

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 def uppercase(str):
-    str = ord(str)
-    if str in range(65, 91):
-        return True
-    else:
-        return False
+    add = ""
+    for l in str:
+        ch = ord(l)
+        if ch in range(97, 123):
+            ch = ch - 32
+            add = add + chr(ch)
+        else:
+            add = add + chr(ch)
+    print("{}".format(add))
