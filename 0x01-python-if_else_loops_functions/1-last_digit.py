f47df33a46b9399cf2fb40ad3f9ccc04aa2ee313
justmogen <justmogen@gmail.com>
2022-11-29 16:32:16 +0000

    checks for last digit

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+lastD = abs(number) % 10  # absolute value of num mod 10 -> gives last digit
+
+print("Last digit of {} is {} and is ".format(number, lastD), end="")
+
+if number < 0:
+    lastD = -lastD   # digit changes to negative
+elif lastD > 5:
+    print("greater than 5")
+elif lastD == 0:
+    print("0")
+else:
+    print("less than 6 and not 0")
