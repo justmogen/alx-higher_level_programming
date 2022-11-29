ee62957297d58b7f8a54ed2d8f1ea7d9cd4648e8
justmogen <justmogen@gmail.com>
2022-11-29 21:19:36 +0000

    print last digits together

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+def print_last_digit(number):
+    print(abs(numnber) % 10, end='')  # find last digit of number and print
+    return (abs(number) % 10)  # return all last digits from numbers
