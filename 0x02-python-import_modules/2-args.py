0c2d4ba613fc4fbe781f15bfc14744194c698602
justmogen <justmogen@gmail.com>
2022-12-01 20:21:04 +0000

    prints the number of and the list of its arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,7 +2,7 @@
 if __name__ == "__main__":
     from sys import argv
 
-    count = len(argv) - 1  #not including filename while countimng
+    count = len(argv) - 1  # not including filename while countimng
     if count == 0:
         print("0 arguments.")
     elif count == 1:
