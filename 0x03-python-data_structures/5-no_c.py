3139e66e8b90f9496bcb0364d0fc740349d9b0c4
justmogen <justmogen@gmail.com>
2022-12-04 20:48:11 +0000

    remove 'c' or 'C' in any word

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 def no_c(my_string):
+    add = ""
     for i in my_string:
-        if i != 'c' or i != 'C':
-            return ("".join(my_string))
+        if i == 'c' or i == 'C':
+            continue
+        add += i
+    return add
