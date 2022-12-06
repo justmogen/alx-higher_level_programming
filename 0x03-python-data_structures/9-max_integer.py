9b247763c15083cfa0493df5c714922c2d9cb91b
justmogen <justmogen@gmail.com>
2022-12-06 19:54:27 +0000

    max number

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -6,5 +6,5 @@ def max_integer(my_list=[]):
         biggest = my_list[0]
         for n in range(len(my_list)):
             if my_list[n] > biggest:
-                biggest = my_list[i]
+                biggest = my_list[n]
         return biggest
