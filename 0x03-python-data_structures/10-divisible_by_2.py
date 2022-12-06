b3266babc487bd94c3b85d3efdc937179a493de8
justmogen <justmogen@gmail.com>
2022-12-06 20:11:10 +0000

    check for divisible of 2

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    for n in range(len(my_list)):
-        if n % 2 == 0:
-            return True
-        else:
-            return False
+    div_two = [True if n % 2 == 0 else False for n in my_list]
+    return div_two
