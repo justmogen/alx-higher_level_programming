1ae59558325ecc2cf91bd7a400668cc60384bef1
justmogen <justmogen@gmail.com>
2022-12-06 19:49:57 +0000

    max number

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if len(my_list) == 0:
+        return (None)
+    else:
+        biggest = my_list[0]
+        for n in range(len(my_list)):
+            if my_list[n] > biggest:
+                biggest = my_list[i]
+        return biggest
