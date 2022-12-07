8d5c0c24a2ad4834c8f7fe05ede230510b1a2c21
justmogen <justmogen@gmail.com>
2022-12-07 21:43:54 +0000

    function that returns a set of common elements in two sets

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    common_value = [value for value in set_1 if value in set_2]
-    return common_value
-print(common_value)
+    return (set_1 & set_2)
