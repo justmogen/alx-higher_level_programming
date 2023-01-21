c11ab3893ecb190113f33e41600153774f1b29c9
justmogen <justmogen@gmail.com>
2023-01-21 19:24:24 +0000

    printing elements

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    numAdd = 0
+    for p in range(x):
+        try:
+            print("{}".format(my_list[p]), end='')
+            numAdd++
+        except IndexError:
+            break
+    print()
+    return (numAdd)
