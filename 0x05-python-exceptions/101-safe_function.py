b048ebd586c2b02c6d9c9c5f24bb0a677bdf9213
justmogen <justmogen@gmail.com>
2023-01-22 05:12:22 +0000

    eecute a function safely

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_function(fct, *args):
+    # func-function to execute
+    # args-arguments for fct
+    # Execute the function safely
+    try:
+        exc_func = fct(*args)
+        return exc_func
+    except:
+        print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
+        return (None)
