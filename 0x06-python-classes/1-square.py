4d59c3dc5749c13d7ad66a57849419ee68be67af
justmogen <justmogen@gmail.com>
2023-01-23 20:46:50 +0000

    create private instance in the aquare class

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 """creating a square class"""
+
+
 class Square:
     """initializing using the special method"""
-    def __init__(self,size):
+    def __init__(self, size):
         """"initializing the square"""
         self.__size = size
