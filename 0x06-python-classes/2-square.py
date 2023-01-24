c7ca8b7d76f5ccd2f5577a31a33f3694c22674b3
justmogen <justmogen@gmail.com>
2023-01-24 16:18:09 +0000

    check for exceptions in our square class and raise errors that occurred

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -4,10 +4,11 @@
 
 class Square:
     """form the square"""
+
     def __init__(self, size=0):
         """check if size is not integer"""
-        if not isinstance(size, int):
+       if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
-            raise ValueError("size must be >= 0)
+            raise ValueError("size must be >= 0")
         self.__size = size
