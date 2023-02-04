01facd9599e2e801d0d18e2e42093f3df036d049
justmogen <justmogen@gmail.com>
2023-02-04 06:57:27 +0000

    functions that divide element of matrix

---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+
+def matrix_divided(matrix, div):
+    """
+    Args:
+        Each row of the matrix must be of the same size
+        matrix must be a list of lists of integers or floats
+        div must be a number (integer or float)
+        div can’t be equal to 0
+
+    Raises:
+        TypeError:
+            matrix must be a matrix (list of lists) of integers/floats
+            Each row of the matrix must have the same size
+            div must be a number
+        ZeroDivisionError:
+            division by zero
+
+    Returns: a new matrix rounded to 2 decimal places
+    """
+
+    result_matrix = []
+
+    output = "matrix must be a matrix (list of lists) of integers/floats"
+    output1 = "Each row of the matrix must have the same size"
+
+    if not isinstance(div, (int, float)):
+        raise TypeError("div must be a number")
+
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+
+    for lists in matrix:
+        if len(lists) != len(matrix[0]):
+            raise TypeError(output1)
+
+        in_matrix = []
+        for nenos in lists:
+            if not isinstance(nenos, (int, float)):
+                raise TypeError(output)
+            else:
+                in_matrix.append(round(nenos / div, 2))
+        result_matrix.append(in_matrix)
+
+    return result_matrix
