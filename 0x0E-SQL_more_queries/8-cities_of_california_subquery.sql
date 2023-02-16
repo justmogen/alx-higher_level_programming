75bfec782b04274dfd069572065f8ed714de1022
justmogen <justmogen@gmail.com>
2023-02-16 18:36:39 +0000

    lists of cities using subquery

---
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,12 @@
+-- lists all the cities of California that can be found in the database hbtn_0d_usa
+/* states table contains only one record where name = California
+	Results must be sorted in ascending order by cities.id
+*/
+SELECT `id`, `name`
+FROM cities
+WHERE `state_id` = (
+	SELECT `id`
+	FROM `states`
+	WHERE name = "California"
+)
+ORDER BY cities.`id` ASC; --ASC is by default even if we not initialize it
