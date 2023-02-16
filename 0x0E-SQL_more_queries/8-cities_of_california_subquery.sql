45f84172ba890403ccb67c77c85902c698243012
justmogen <justmogen@gmail.com>
2023-02-16 18:50:51 +0000

    lists of cities using subquery

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -2,11 +2,9 @@
 /* states table contains only one record where name = California
 	Results must be sorted in ascending order by cities.id
 */
-SELECT cities*.
+SELECT `id`, `name`
 FROM cities, states
-WHERE cities.`state_id` IN (
-	SELECT `id`
-	FROM `states`
-	WHERE `name` = 'California'
-)
-ORDER BY cities.`id`; --ASC is by default even if we not initialize it
+WHERE cities.state_id = states.id
+  AND states.name = 'California'
+
+ORDER BY `id` ASC; --ASC is by default even if we not initialize it
