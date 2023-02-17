86c7226c985c038e648032ea37b8cea6cec87589
justmogen <justmogen@gmail.com>
2023-02-17 19:42:37 +0000

    cities.id - cities.name - states.name display format

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -4,7 +4,7 @@
  You can use only one SELECT statement
 */
 
-SELECT `cities.id`, `cities.name`, `states.name`
-FROM cities, states
-WHERE `cities.state_id` = `states.id`
-ORDER BY `cities.id` ASC;
+SELECT cities.`id`, cities.`name`, states.`name`
+FROM `cities`, `states`
+WHERE cities.`state_id` = states.`id`
+ORDER BY cities.`id` ASC;
