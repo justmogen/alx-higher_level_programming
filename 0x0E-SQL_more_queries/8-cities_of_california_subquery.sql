af12d4fd0887c52555363984034a12253e9e03bd
justmogen <justmogen@gmail.com>
2023-02-17 19:20:15 +0000

    correcfion

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -2,9 +2,9 @@
 /* states table contains only one record where name = California
 	Results must be sorted in ascending order by cities.id
 */
-SELECT `id`, `name`
-FROM cities, states
-WHERE cities.state_id = states.id
-  AND states.name = 'California'
-
-ORDER BY `id` ASC; --ASC is by default even if we not initialize it
+SELECT * FROM `cities`
+WHERE `state_id` = (
+	SELECT `id` FROM `states`
+	WHERE `name` = California
+)
+ORDER BY `id` ASC;
