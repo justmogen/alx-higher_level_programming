30463b3137dfa644e9613a68edd9b72354fd4468
justmogen <justmogen@gmail.com>
2023-02-17 19:48:02 +0000

    cities.id - cities.name - states.name display format

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -5,7 +5,7 @@
 */
 
 SELECT c.`id`, c.`name`, s.`name`
-	FROM `cities` AS c
-		INNER JOIN `states` AS s
-		ON c.`states_id` = s.`id`
+FROM `cities` AS c
+INNER JOIN `states` AS s
+ON c.`states_id` = s.`id`
 ORDER BY c.`id` ASC;
