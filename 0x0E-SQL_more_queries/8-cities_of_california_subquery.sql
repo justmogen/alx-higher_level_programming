4d7d3efb121e2c8757410c5f2e569f605ffec15f
justmogen <justmogen@gmail.com>
2023-02-17 19:25:20 +0000

    correction

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -2,6 +2,7 @@
 /* states table contains only one record where name = California
 	Results must be sorted in ascending order by cities.id
 */
+USE hbtn_0d_usa;
 SELECT * FROM `cities`
 WHERE `state_id` = (
 	SELECT `id` FROM `states`
