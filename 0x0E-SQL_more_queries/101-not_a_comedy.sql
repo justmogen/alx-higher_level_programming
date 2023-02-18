26435609f6b83f0ded2273e60f6f23a5b4886db7
justmogen <justmogen@gmail.com>
2023-02-18 09:37:11 +0000

    lists all shows without genre comedy

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -5,10 +5,11 @@
 
 SELECT tv_shows.title
 FROM tv_shows
-WHERE tv_shows_id NOT IN (
-	SELECT tv_show_genres.show_id
-	FROM tv_show_genres
-	JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
+WHERE tv_shows.id NOT IN (
+	SELECT tv_shows.id
+	FROM tv_shows
+	JOIN tv_show_genres ON tv_shows.id = tv_show_genres.genre_id
+	JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
 	WHERE tv_genres.name = 'Comedy'
 )
 ORDER BY tv_shows.title ASC;
