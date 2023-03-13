2546add5fa3178028e27ae496cf81d7abd6f2205
justmogen <justmogen@gmail.com>
2023-03-14 00:13:46 +0300

    corrections

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,17 +1,2 @@
 #!/usr/bin/node
-
-const process = require('process');
-
-const args = process.argv.slice(2);
-/*
- * script checks whether the first element of the args array is truth
- * (i.e., not null, undefined, false, 0, or an empty string).
- * If it is, we print the first argument using console.log(...).
- * Otherwise, we print "No argument".
- */
-
-if (args[0]) {
-  console.log(args[0]);
-} else {
-  console.log('No arguments');
-}
+console.log(typeof process.argv[2] === 'undefined' ? 'No argument' : process.argv[2]);
--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,11 +1,2 @@
 #!/usr/bin/node
-
-const process = require('process');
-const args = process.argv.slice(2);
-
-if (args.length === 2) {
-  const formatArgs = args.join(' is ');
-  console.log(formatArgs);
-} else {
-  console.log('Two arguments are required');
-}
+console.log(`${process.argv[2]} is ${process.argv[3]}`);
