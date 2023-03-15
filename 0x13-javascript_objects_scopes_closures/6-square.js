db537dd1b312458225ac8f680206a4707cb81da3
justmogen <justmogen@gmail.com>
2023-03-15 22:47:26 +0300

    inherits..

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+
+module.exports = class Square extends require('./5-square.js') {
+  charPrint (c) {
+    if (c === undefined) {
+      this.print();
+    } else {
+      for (let n = 0; n < this.height; n++) { console.log(c.repeat(this.width)); }
+    }
+  }
+};
