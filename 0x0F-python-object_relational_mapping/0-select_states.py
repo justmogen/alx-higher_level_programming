f77fb6a4ab173e20ff55f73ec1c3d5ceb83dc7ce
justmogen <justmogen@gmail.com>
2023-03-21 19:03:01 +0300

    correction

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,11 +1,42 @@
 #!/usr/bin/python3
-
-# Lists all states from the database hbtn_0e_0_usa.
+# Displays all values in the states table of hbtn_0e_0_usa where name matches the argument.
 
 import sys
 import MySQLdb
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    c.execute("SELECT * FROM `states`")
-    [print(state) for state in c.fetchall()]
+    # Check the number of arguments
+    if len(sys.argv) != 5:
+        print("Usage: {} username password database state_name".format(sys.argv[0]))
+        sys.exit(1)
+
+    # Connect to the database
+    try:
+        db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            db=sys.argv[3]
+        )
+    except MySQLdb.Error as e:
+        print("Error connecting to MySQL database: {}".format(e))
+        sys.exit(1)
+
+    # Create a cursor object
+    cursor = db.cursor()
+
+    # Execute the SQL query
+    query = "SELECT * FROM states WHERE name LIKE BINARY %s ORDER BY id ASC"
+    state_name = sys.argv[4]
+    cursor.execute(query, ('%{}%'.format(state_name),))
+
+    # Fetch the results and print them
+    results = cursor.fetchall()
+    for row in results:
+        print(row)
+
+    # Close the cursor and database connection
+    cursor.close()
+    db.close()
+
