c0d87144aa24e53b023b3e98069de1dee5b14646
Black Bourne <black@mogen.0.2.15>
2023-04-04 13:24:42 +0300

    hat takes in a URL and an email, sends a POST request to the passed URL with the email as a parameter, and displays the body of the response (decoded in utf-8)

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -3,13 +3,14 @@
 Takes in a URL, sends a request to the URL and displays the value
 of the X-Request-Id variable found in the header of the response
 
-Usage: ./fetch_resquest_id.py <URL>
+Usage: ./post_email.py <URL> <email>
 
 Argument:
     <URL>: the url to fetch
+    <email>: email address to include int he POST requests
 
 Returns:
-    The value of the X-Request-Id variable
+    The body of the response from the server (decoded in utf-8).
 """
 
 
