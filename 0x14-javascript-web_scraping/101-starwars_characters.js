8c07c936f322c94e221758538b9ba0c68b6c2d2d
justmogen <justmogen@gmail.com>
2023-07-27 13:18:57 +0300

    trying a different sequence

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,27 +1,19 @@
 #!/usr/bin/node
 
 const request = require('request');
-const apiUrl = `https://swapi.dev/api/films/${process.argv[2]}/`;
+const apiUrl = `https://swapi-api.alx-tools.com/api/films/${process.argv[2]}/`;
 
 request(apiUrl, function (error, response, body) {
   if (!error && response.statusCode === 200) {
     const charactersUrls = JSON.parse(body).characters;
-    let fetchedCharacters = 0;
 
     const fetchCharacterName = (characterUrl) => {
       request(characterUrl, function (error, response, body) {
         if (!error && response.statusCode === 200) {
           const characterData = JSON.parse(body);
           console.log(characterData.name);
-
-          fetchedCharacters++;
-          if (fetchedCharacters === charactersUrls.length) {
-            // All characters fetched, exit the script
-            process.exit(0);
-          }
         } else {
           console.error(`Error: Failed to retrieve character data. Status code: ${response.statusCode}`);
-          process.exit(1);
         }
       });
     };
@@ -29,6 +21,5 @@ request(apiUrl, function (error, response, body) {
     charactersUrls.forEach(fetchCharacterName);
   } else {
     console.error(`Error: Failed to retrieve movie data. Status code: ${response.statusCode}`);
-    process.exit(1);
   }
 });
